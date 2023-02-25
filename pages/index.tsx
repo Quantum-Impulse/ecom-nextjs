77e003d19a5aa71776ecf1d3d6e32e23ea63ef47
Enrique Rivera <erivera7240@gmail.com>
2023-02-25 07:37:49 -0600

    adding files before conitueing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 const index = () => {
-  const message:string = "Hello World"
+  const message: string = "Hello World"
+  const age: number = 12 ;
+  let b: NodeJS.Process 
+
+  let person: Person = {
+    name: "James"
+  }
+
+
   return (
     
     <div>{message}</div>
