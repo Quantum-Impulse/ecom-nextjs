1e4ad78fbde28e24af1fc080f4a457b7df32e963
Enrique Rivera <erivera7240@gmail.com>
2023-03-16 21:05:28 -0500

    interface vs types

--- playground.ts
+++ playground.ts
@@ -1,29 +1,34 @@
-export default function play() {
-    
-    console.log("Hello World")
+interface Person {
+    name: string,
+    age: number
+}
 
+// type Person ={
+//     name: string,
+//     age: number
+// }
+
+export default function play() {
     const name: string = 'Filip'
     const age: number = 30
 
-    const person = {
+    const person:Person = {
         name: "John",
         age: 43
     }
-
+    
     function logPersonInfo( personName: string, personAge: number){
         const info = "Name: " + personName + ", age: " + personAge
         
         console.log(info)
         return info
     }
-    function logPersonInfo2(person: {name: string, age: number}){
+    function logPersonInfo2(person: Person){
         const info = "Name: " + person.name + ", age: " + person.age
         
         console.log(info)
         return info
     }
-
-
 
     logPersonInfo(name, age)
     logPersonInfo2(person)
