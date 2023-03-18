2dd0bfab9abd4625bd845d58df9db8b5b7d1b6e6
Enrique Rivera <erivera7240@gmail.com>
2023-03-18 08:56:28 -0500

    17. extends

--- playground.ts
+++ playground.ts
@@ -1,17 +1,34 @@
-
-
-export default function play() {
-
-
-    const random = Math.random() > 0.5 ? "Hello String" : [1,2]
-
-    // this is narrowing, is asking which type of var type is a var before changing/using it
-    if (typeof random == "string") {
-        const upper = random.toUpperCase()
-    } else {
-        console.log(random)
+interface Person {
+    name: string
+    age: number
+  }
+  
+  interface BusinessPerson extends Person {
+    salary: number
+  }
+  
+  interface AcademicPerson extends Person {
+    publications: string[]
+  }
+  
+  
+  
+  
+  export default function play() {
+    const person: AcademicPerson = {
+      name: "filip",
+      age: 23,
+      publications: ["1", "2"]
     }
-
-    
-
-}
+  
+    const person2: BusinessPerson = {
+      name: "filip",
+      age: 23,
+      salary: 1000
+    }
+  
+    function logPerson(person: Person) {}
+  
+  
+    logPerson(person2)
+  }
