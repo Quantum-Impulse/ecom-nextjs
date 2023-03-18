6923785d699148202035d1401e310aa89b0e0208
Enrique Rivera <erivera7240@gmail.com>
2023-03-18 08:50:36 -0500

    16. norrowing review

--- playground.ts
+++ playground.ts
@@ -1,48 +1,17 @@
-// interface Person {
-//     name: string,
-//     age: number
-// }
-class Person {
-    name: string = ""
-    age?: number = 0
 
-    constructor (name: string, age: number) {
-        this.name = name
-        this.age = age
-    }
-}
-
-// interface PersonLoggerFn {
-//     (name:string, age:number): string
-// }
-
-type PersonLoggerFn = (name:string, age?:number) => string
 
 export default function play() {
-    const name: string = 'Filip'
-    const age: number = 30
 
-    const john:Person = {
-        name: "John",
-        
+
+    const random = Math.random() > 0.5 ? "Hello String" : [1,2]
+
+    // this is narrowing, is asking which type of var type is a var before changing/using it
+    if (typeof random == "string") {
+        const upper = random.toUpperCase()
+    } else {
+        console.log(random)
     }
 
-    const logPersonInfo:PersonLoggerFn = ( 
-        personName:string, personAge:number  = 0
-    ): string =>{
-        const info = "Name: " + personName + ", age: " + personAge
-        console.log(info)
-        return info
-    }
-
-    function logPersonInfo2(person: Person){
-        const info = "Name: " + person.name + ", age: " + person.age
-        
-        console.log(info)
-        return info
-    }
-
-    const log:string = logPersonInfo(name)
-    logPersonInfo2( new Person("bruh", 40))
+    
 
 }
