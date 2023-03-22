d47251a002778c0d0290fe61800e52a14e1a2174
Enrique Rivera <erivera7240@gmail.com>
2023-03-22 12:35:37 -0500

    53. fetch product query done

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,21 +1,31 @@
 
-const fetchApi = async () => {
-    const url = "https://jsonplaceholder.typicode.com/todos"
+import getAllProductQuery from "../utils/queries/get-all-products"
 
-    const res = await fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-
-    const data = await res.json()
-    return { data }
+type FetchParams = {
+  query: string
 }
 
+const fetchApi = async ({query}: FetchParams) => {
+  const url = "http://localhost:4000/graphql"
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query
+    })
+  })
+
+  const data = await res.json()
+  return { data }
+}
+
+
 const getAllProducts = async (): Promise<any[]> => {
-    const products = await fetchApi()
-    return products.data
+  const products = await fetchApi({query: getAllProductQuery})
+  return products.data
 }
 
 export default getAllProducts
