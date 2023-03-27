264cf40f68435e2b3d6cb1b161948577be93885b
Enrique Rivera <erivera7240@gmail.com>
2023-03-26 22:23:04 -0500

    next js config files for each framework

---
+++ next.config.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+    i18n: {
+        locales: ["en-US", "es"],
+        defaultLocale: "en-US"
+    }
+}
+
+console.log("next-config:", JSON.stringify(module.exports, null, 2))
