4268e7d849e437ad93a01ff27d98b97ea80ce58b
Enrique Rivera <erivera7240@gmail.com>
2023-03-27 12:37:01 -0500

    ts config specific setup issues solved

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,14 +1,27 @@
 const path = require("path")
+const fs = require("fs")
 const merge = require("deepmerge")
-const { ModifierFlags } = require("typescript")
 
-function withFrameworkConfig( defaultConfig ={} ) {
-    const framework = "shopify" // change to frameworking you want to use
+function withFrameworkConfig( defaultConfig = {} ) {
+  const framework = "shopify" 
 
-    const frameworkNextConfig = require(path.join("../", framework, "next.config"))
-    const config = merge(defaultConfig, frameworkNextConfig)
+  const frameworkNextConfig = require(path.join("../", framework, "next.config"))
+  const config = merge(defaultConfig, frameworkNextConfig)
 
-    return config
+  const tsPath = path.join(process.cwd(), "tsconfig.json")
+  const tsConfig = require(tsPath)
+
+  tsConfig.compilerOptions.paths["@framework"] = [`framework/${framework}`]
+  tsConfig.compilerOptions.paths["@framework/*"] = [`framework/${framework}/*`]
+
+  fs.writeFileSync(
+    tsPath,
+    JSON.stringify(
+      tsConfig, null, 2
+    )
+  )
+
+  return config
 }
 
 module.exports = {withFrameworkConfig}
