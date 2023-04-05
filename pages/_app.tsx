feb8d1e274acef09594c4cf70fc7e43b915c8245
Enrique Rivera <erivera7240@gmail.com>
2023-04-05 17:47:16 -0500

    70. adding _app component

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+
+import {AppProps } from  "next/app"
+
+function MyApp({Component, pageProps}: AppProps){
+    return (
+        <div className="app-component">
+            <Component {...pageProps} />
+        </div>
+    )
+}
+
+export default MyApp
