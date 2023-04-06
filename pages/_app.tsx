e36d29c37cadefd54eec243b45a539d271c5d573
Enrique Rivera <erivera7240@gmail.com>
2023-04-06 10:07:56 -0500

    71. Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 
 import {AppProps } from  "next/app"
+import { Layout } from "@components/common"
 
 function MyApp({Component, pageProps}: AppProps){
     return (
-        <div className="app-component">
+        <Layout>
             <Component {...pageProps} />
-        </div>
+        </Layout>
     )
 }
 
