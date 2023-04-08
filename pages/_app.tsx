53c72c587c473ec95f8dc7bdfb433dbc5c8690d8
Enrique Rivera <erivera7240@gmail.com>
2023-04-08 01:30:29 -0500

    74. NOOP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
 
-import {AppProps } from  "next/app"
-import { Layout } from "@components/common"
+import { AppProps } from "next/app"
+import { FC } from "react"
 
-function MyApp({Component, pageProps}: AppProps){
-    return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    )
+interface Layout {
+    children: React.ReactNode;
+  } 
+const Noop: React.FC<Layout> = ({children}) => <>{children}</>
+
+function MyApp({Component, pageProps}: AppProps & {Component: {Layout: FC}}) {
+  const Layout = Component.Layout ?? Noop
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
