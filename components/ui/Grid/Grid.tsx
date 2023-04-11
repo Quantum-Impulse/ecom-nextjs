3bb10c0ac528f2cc622d0cfb9baeced669367ebf
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2023-04-11 09:23:49 -0500

    87. grid stylings

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,11 +1,13 @@
 
+import s from "./Grid.module.css"
+
 interface Grid {
     children: React.ReactNode;
   }
 
 const Grid: React.FC<Grid> = ({children}) => {
     return (
-        <div className="root-grid">
+        <div className={s.root}>
             {children}
         </div>
     )
