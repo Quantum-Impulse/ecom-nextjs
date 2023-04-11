9e39b300034e451a107a387f727a073211c73ea5
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2023-04-11 08:56:46 -0500

    85. Product info

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,14 +1,37 @@
 import { Product } from "@common/types/product"
+import Link from "next/link"
+import Image from "next/image"
 
 interface Props {
-    product?: Product
+    product: Product
 }
-
+const placeholderImage="/product-image-placeholder.svg"
 const ProductCard: React.FC<Props> = ({product}) => {
 
     
     return(
-        <div>{product.name}</div>
+        <Link href={`/products/${product.slug}`}> 
+            <a>
+                <h3>
+                    <span>
+                        <div>{product.name}</div>
+                    </span>
+                </h3>
+                <span>$13</span>
+                {product.images && (
+                    <Image 
+                        alt={product.name} 
+                        src={placeholderImage} 
+                        height={540} 
+                        width={540}
+                        quality="85" 
+                        layout="responsive"
+                    />
+                )
+                }
+                
+            </a>
+        </Link>
     )
 }
 
