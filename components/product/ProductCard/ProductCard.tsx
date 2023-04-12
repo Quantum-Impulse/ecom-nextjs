12a18ad9eb6d05eb54d350d9db9af7d94b1605bd
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2023-04-12 14:34:18 -0500

    94. css product tag

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,37 +1,38 @@
 import { Product } from "@common/types/product"
+import { FC } from "react"
 import Link from "next/link"
 import Image from "next/image"
+import s from "./ProductCard.module.css"
 
 interface Props {
-    product: Product
+  product: Product
 }
-const placeholderImage="/product-image-placeholder.svg"
+const placeholderImage = "/product-image-placeholder.svg"
+const ProductCard: FC<Props> = ({product}) => {
 
-const ProductCard: React.FC<Props> = ({product}) => {
-    return(
-        <Link href={`/products/${product.slug}`}> 
-            <a>
-                <h3>
-                    <span>
-                        <div>{product.name}</div>
-                    </span>
-                </h3>
-                <span>$13</span>
-                {product.images && (
-                    <Image 
-                        alt={product.name} 
-                        src={product.images[0].url ?? placeholderImage} 
-                        height={540} 
-                        width={540}
-                        quality="85" 
-                        layout="responsive"
-                    />
-                )
-                }
-                
-            </a>
-        </Link>
-    )
+  return (
+    <Link href={`/products/${product.slug}`}>
+      <a className={s.root}>
+        <div className={s.productTag}>
+          <h3 className={s.productTitle}>
+            <span>{product.name}</span>
+          </h3>
+          <span className={s.productPrice}>14 $</span>
+        </div>
+        { product.images && (
+          <Image
+            alt={product.name ?? "Product image"}
+            src={product.images[0].url ?? placeholderImage}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )
+        }
+      </a>
+    </Link>
+  )
 }
 
 export default ProductCard
