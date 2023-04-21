ad595baa9b6293411d40b1e8701c685974ec1f1c
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2023-04-21 09:49:56 -0500

    112. Product Variant

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,8 @@ export default function Home({
       <Grid>
         { products.slice(0,3).map(product => 
           <ProductCard 
-            key={product.id} 
+            key={product.id}
+            variant="slim" 
             product={product}
           />
         )}
