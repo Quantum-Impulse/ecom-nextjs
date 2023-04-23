dab322f54890372fa4ffef2e60f4f4b3c4fba767
Enrique Rivera <erivera7240@gmail.com>
2023-04-23 13:04:43 -0500

    115. Layout B

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home({
 
   return (
     <>
-      <Grid>
+      <Grid layout="B">
         { products.slice(0,3).map(product => 
           <ProductCard 
             key={product.id}
@@ -44,6 +44,14 @@ export default function Home({
           <ProductCard key={product.id} product={product}/>  
         )}
       </Marquee>
+      <Grid layout="B">
+        { products.slice(0,3).map(product =>
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        )}
+      </Grid>
     </>
   )
 }
