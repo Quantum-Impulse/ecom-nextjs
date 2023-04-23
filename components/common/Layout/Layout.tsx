99691a26ff87cf374a818c3b9f6a5230734c2fa1
Enrique Rivera <erivera7240@gmail.com>
2023-04-23 13:55:20 -0500

    119. Navbar links

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,15 +1,21 @@
+import s from "./Layout.module.css"
+import { Footer, Navbar } from "@components/common"
+
+
 interface LayoutProps {
     children: React.ReactNode;
   }
 
-import s from "./Layout.module.css"
+
    
   const Layout: React.FC<LayoutProps> = ({ children }) => {
     return( 
       <div className={s.root}>
+        <Navbar/>
         <main className="fit">
           { children }
         </main>
+        <Footer/>
       </div>
     )
   };
