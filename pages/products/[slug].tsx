a6a72e5a5e65f189750504bbd4947dd500315bac
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2023-05-02 11:01:39 -0500

    132. get all products paths

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,14 +1,17 @@
 import { Layout } from "@components/common"
+import { getConfig } from "@framework/api/config"
+import getAllProducts from "@framework/product/get-all-products"
+import { get } from "http"
 import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next"
 
 // fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
+
+  const config = getConfig()
+  const { products } = await getAllProducts(config)
+
   return {
-    paths: [
-      { params: { slug: "cool-hat"}},
-      { params: { slug: "t-shirt"}},
-      { params: { slug: "lightweight-jacket"}}
-    ],
+    paths: products.map(p => ( {params: {slug: p.slug} } )),
     fallback: false
   }
 }
