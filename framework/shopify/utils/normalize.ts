b311cd5651ae204c010e20b723f8a4abc7c01d08
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2023-05-06 13:57:52 -0500

    139. normalize options

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,50 +1,65 @@
 
-import  {
+
+import {
     ImageEdge,
     MoneyV2,
-    Product as ShopifyProduct
-} from "../schema"
-
-import { Product } from "@common/types/product"
-
-const normalizeProductImages = ({edges}: { edges: Array<ImageEdge>} ) =>
-    edges.map( ({node: {originalSrc: url, ...rest}}) => (
-        {
-            url: `/images/${url}`,
-            ...rest
-        }
+    Product as ShopifyProduct,
+    ProductOption
+  } from "../schema"
+  
+  import { Product } from "@common/types/product"
+  
+  const normalizeProductImages = ({edges}: {edges: Array<ImageEdge>}) =>
+    edges.map(({node: { originalSrc: url, ...rest}}) => ({
+        url: `/images/${url}`,
+        ...rest }
     ))
-
-    const normalizeProductPrice = ({currencyCode, amount}: MoneyV2) => ({
-        value: +amount, // transform string to number with the +
-        currencyCode
-    })
-
-
-
-export function normalizeProduct(productNode: ShopifyProduct): Product {
+  
+  const normalizeProductPrice = ({currencyCode, amount}: MoneyV2) => ({
+    value: +amount,
+    currencyCode
+  })
+  
+  const normalizeProductOption = ({
+    id,
+    values,
+    name: displayName
+  }: ProductOption) => {
+  
+    console.log("ID ", id)
+    console.log("NAME ", displayName)
+    console.log("VALUES ", values)
+  
+    return {}
+  }
+  
+  export function normalizeProduct(productNode: ShopifyProduct): Product {
     const {
-        id,
-        title: name,
-        handle,
-        vendor,
-        description,
-        images: imageConnection,
-        priceRange,
-        ...rest
+      id,
+      title: name,
+      handle,
+      vendor,
+      description,
+      images: imageConnection,
+      priceRange,
+      options,
+      ...rest
     } = productNode
-
+  
     const product = {
-        id,
-        name, 
-        vendor,
-        description,
-        path: `/${handle}`,
-        slug: handle.replace( /^\/+|\/+$/g, ""), // regrex expression to remove
-        images: normalizeProductImages(imageConnection),
-        price: normalizeProductPrice(priceRange.minVariantPrice),
-        ...rest
+      id,
+      name,
+      vendor,
+      description,
+      path: `/${handle}`,
+      slug: handle.replace(/^\/+|\/+$/g, ""),
+      images: normalizeProductImages(imageConnection),
+      price: normalizeProductPrice(priceRange.minVariantPrice),
+      options: options ?
+        options.filter(o => o.name !== "Title")
+               .map(o => normalizeProductOption(o)) : [],
+      ...rest
     }
-
+  
     return product
-}
+  }
