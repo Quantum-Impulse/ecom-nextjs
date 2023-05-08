93765762815c4b517d5320c8500fe42c35dcfea0
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2023-05-08 14:49:29 -0500

    fixed major issues for 145, nx is css

---
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from "react"
+import s from "./ProductSlider.module.css"
+
+interface Props {
+    children: React.ReactNode
+  }
+  
+
+const ProductSlider: FC<Props> = ({children}) => {
+
+  return (
+    <div className={s.root}>
+      <div className="h-full transition-opacity">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default ProductSlider
