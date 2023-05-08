a30841c4e2f2d884dd815870e23d1aaaf731729e
Enrique Rivera Jr <46612795+Quantum-Impulse@users.noreply.github.com>
2023-05-08 14:02:33 -0500

    added fix in tailwind.config file, mk more fixes

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -8,6 +8,8 @@ import { GetStaticPaths,
   GetStaticPropsContext, 
   InferGetStaticPropsType } from "next"
 import { Container } from "@components/ui"
+import { ProductView } from "@components/product"
+
 
 
 // fetch all of the products slugs
@@ -42,47 +44,9 @@ export default function ProductSlug({
   product }: InferGetStaticPropsType<typeof getStaticProps>
 ) {
   return (
-    <Container>
-      <p>id: {product?.id}</p>
-      <p>name: {product?.name}</p>
-      <p>price value: {product?.price.value}</p>
-      <p>price currency: {product?.price.currencyCode}</p>
-      <p>description: {product?.description}</p>
-
-      <h1 className="mb-4">OPTIONS</h1>
-      <div>
-        { product?.options.map(option =>
-          <div>
-            <p>Name: {option.displayName}</p>
-            { option.values.map(value =>
-              <div>
-                <p>Label: {value.label}</p>
-                <p>Hex Color: {value.hexColor}</p>
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-      <h1 className="mb-4">VARIANTS</h1>
-      <div>
-        { product?.variants.map(variant =>
-          <div>
-            <p>Variant Name: {variant.name}</p>
-            { variant.options.map(vo =>
-              <div>
-                <p>Name: {vo.displayName}</p>
-                { vo.values.map(value =>
-                  <div>
-                    <p>Label: {value.label}</p>
-                    <p>Hexcolor: {value.hexColor}</p>
-                  </div>
-                )}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-  </Container>
+    <>
+      { product && <ProductView product={product} />}
+    </>
     
   )
 }
