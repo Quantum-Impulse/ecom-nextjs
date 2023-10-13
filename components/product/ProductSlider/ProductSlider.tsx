7836a077f6cf0603cc79e1f4fa22cab51cf1c775
Enrique Rivera <erivera7240@gmail.com>
2023-10-13 01:11:16 -0500

    146. adding keen slider

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,17 +1,28 @@
-import { FC, ReactNode } from "react"
+
+import React, { FC, Children, isValidElement } from "react"
 import s from "./ProductSlider.module.css"
 
-interface Props {
-    children: React.ReactNode
-  }
-  
-
-const ProductSlider: FC<Props> = ({children}) => {
+const ProductSlider: FC = ({children}) => {
 
   return (
     <div className={s.root}>
-      <div className="h-full transition-opacity">
-        {children}
+      <div className="keen-slider h-full transition-opacity">
+        { Children.map(children, child => {
+          if (isValidElement(child)) {
+            // return {
+            //   ...child,
+            //   props: {
+            //     ...child.props,
+            //     className: "keen-slider__slide"
+            //   }
+            // }
+
+            return React.cloneElement(child, {className: "keen-slider__slide"})
+          }
+
+          return child
+        }
+        )}
       </div>
     </div>
   )
